7369eb11a06f79d5497681e0187cf93da9f98cf6
JP Fabrero <111621084+jpK1ba@users.noreply.github.com>
2023-03-12 13:25:50 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -2,7 +2,7 @@ import os
 import matplotlib.pyplot as plt
 from IPython.display import HTML
 
-def center_fig(fig, name):
+def center_fig(fig, name, width=80):
     """Saves the current figure into an image and center-display
     """
     # Save the plot as a PNG file
@@ -19,5 +19,6 @@ def center_fig(fig, name):
     fig.savefig(fp) # Create an HTML img tag to display the image
     img_tag = (f'<img src="{fp}" alt="plots"'
                'style="display:block; margin-left:auto;'
-               'margin-right:auto;width:80%;">') # Display the img tag in the Jupyter Notebook
+               f'margin-right:auto;width:{width}%;">') # Display the img tag in the Jupyter Notebook
     display(HTML(img_tag))
+    plt.close(fig)
